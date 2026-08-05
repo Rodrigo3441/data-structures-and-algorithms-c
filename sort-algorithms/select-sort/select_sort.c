85e679c6f8641debc50a556da08471692ba0fd30
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-04 22:11:42 -0300

    add select sort implementation with header file

---
+++ sort-algorithms/select-sort/select_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int select_sort(int *arr, int size){
+	
+	int total_steps = 0;
+    int total_swaps = 0;
+    
+    for (int i = 0; i < size - 1; i++){
+    	
+    	int lowest = i;
+    	
+    	for (int j = i + 1; j < size - 1; j++){
+    		total_steps++;
+    		
+    		if (arr[j] < arr[lowest]){
+    			lowest = j;
+    			printf("New lowest value found: %d\n", lowest);
+			}
+				
+		}
+		
+		int temp = arr[i];
+		arr[i] = arr[lowest];
+		arr[lowest] = temp;
+		
+		total_swaps++;
+		
+	}
+	
+	printf("The array has been sorted in ascending order using the Select Sort algorithm.\n");
+    printf("Total steps: %d\n", total_steps);
+    printf("Total swaps: %d\n", total_swaps);
+	
+}
---
+++ sort-algorithms/select-sort/select_sort.h
@@ -0,0 +1,6 @@
+#ifndef SELECT_SORT_H
+#define SELECT_SORT_H
+
+int select_sort(int *arr, int size);
+
+#endif
