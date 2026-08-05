1d3ca235177d153e6bcbc14196065674a6bd0dac
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-04 22:12:42 -0300

    add first insertion sort code draft

---
+++ sort-algorithms/insertion-sort/insertion_sort.c
@@ -0,0 +1,29 @@
+/*
+This file is currently under construction
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "insertion_sort.h"
+
+int insertion_sort(int *arr, int size){
+
+    int total_steps = 0;
+    int total_swaps = 0;
+
+    for (int i = 0; i < size - 1; i++){
+        
+        for (int j = 0; j < size - i -1; j++){
+          
+            
+            
+        }
+
+    }
+
+    printf("The array has been sorted in ascending order using the Insertion Sort algorithm.\n");
+    printf("Total steps: %d\n", total_steps);
+    printf("Total swaps: %d\n", total_swaps);
+    
+    return 0;
+}
