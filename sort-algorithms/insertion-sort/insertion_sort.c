6b9bba90b7ef72ef4264c245443458166456e689
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-05 21:44:24 -0300

    add implementation of insertion sort

--- sort-algorithms/insertion-sort/insertion_sort.c
+++ sort-algorithms/insertion-sort/insertion_sort.c
@@ -1,7 +1,3 @@
-/*
-This file is currently under construction
-*/
-
 #include <stdio.h>
 #include <stdbool.h>
 #include "insertion_sort.h"
@@ -11,16 +7,20 @@ int insertion_sort(int *arr, int size){
     int total_steps = 0;
     int total_swaps = 0;
 
-    for (int i = 0; i < size - 1; i++){
-        
-        for (int j = 0; j < size - i -1; j++){
-          
-            
-            
-        }
-
-    }
-
+  	for (int i = 1; i < size; i++){
+  		int key = arr[i];
+  		int j = i - 1;
+  		
+  		while (j >= 0 && arr[j] > key){
+  			total_steps++;
+  			total_swaps++;
+  			
+  			arr[j+1] = arr[j];
+  			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+	
     printf("The array has been sorted in ascending order using the Insertion Sort algorithm.\n");
     printf("Total steps: %d\n", total_steps);
     printf("Total swaps: %d\n", total_swaps);
