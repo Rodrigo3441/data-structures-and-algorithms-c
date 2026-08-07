64e418c3905e483915fd0ba09728f6ea9e300e7e
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-06 23:06:29 -0300

    add header file for merge sort implementation

---
+++ sort-algorithms/merge-sort/merge_sort.h
@@ -0,0 +1,10 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+int merge_sort(int *arr, int left, int right);
+void print_division_status(
+			int *arr, 
+			int left_index,
+			int right_index);
+
+#endif
