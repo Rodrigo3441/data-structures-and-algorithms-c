f28b40108d99854d8105d1525330d98c9558c3b6
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-06 23:06:14 -0300

    implement merge sort in main function and clean up unused variables

--- sort-algorithms/main.c
+++ sort-algorithms/main.c
@@ -2,8 +2,9 @@
 #include "bubble-sort/bubble_sort.h"
 #include "select-sort/select_sort.h"
 #include "insertion-sort/insertion_sort.h"
+//#include "merge-sort/merge_sort.h"
 
-void print_array(int *arr, int size){
+void print_array(int *arr, int size) {
     for (int i = 0; i < size; i++){
         printf("%d -> ", arr[i]);
     }
@@ -15,14 +16,11 @@ int main(void) {
     int size = 20;
 
     int values[20] = {84, 12, 67, 45, 93, 29, 51, 78, 36, 90, 14, 62, 73, 5, 88, 41, 99, 27, 50, 61};
-	int values2[20] = {84, 12, 67, 45, 93, 29, 51, 78, 36, 90, 14, 62, 73, 5, 88, 41, 99, 27, 50, 61};
-	int values3[20] = {84, 12, 67, 45, 93, 29, 51, 78, 36, 90, 14, 62, 73, 5, 88, 41, 99, 27, 50, 61};
-//    print_array(values, size);
+    print_array(values, size);
 
-	select_sort(values, size);
-    insertion_sort(values2, size);
-    bubble_sort(values3, size);
-//    print_array(values, size);
+	merge_sort(values, 0, size);
+
+
 
     return 0;
 }
