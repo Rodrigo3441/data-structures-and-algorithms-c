f79cc480a0a1acfbeaae9c39eb3e4e4ceb5facf7
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-07 20:04:38 -0300

    Change return type of merge_sort function to void

--- sort-algorithms/merge-sort/merge_sort.h
+++ sort-algorithms/merge-sort/merge_sort.h
@@ -1,10 +1,11 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
-int merge_sort(int *arr, int left, int right);
+void merge_sort(int *arr, int left, int right);
 void print_division_status(
 			int *arr, 
 			int left_index,
 			int right_index);
+void merge(int *arr, int left_index, int right_index, int medium);
 
 #endif
